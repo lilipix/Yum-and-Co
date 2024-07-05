3949bb5b822924275f95bf3dc10d803fe14bb207
Aurélie <aurelie.demure.ad@gmail.com>
2024-07-05 12:09:27 +0200

    update connect to database

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,12 +1,39 @@
 import mongoose from "mongoose";
 
-const myEnvVar = process.env.MONGODB_URI as string;
+// const myEnvVar = process.env.MONGODB_URI as string;
 
+// const connectToDatabase = async () => {
+//   try {
+//     await mongoose.connect(myEnvVar, { serverSelectionTimeoutMS: 50000 });
+//   } catch (error) {
+//     console.error("Failed to connect on database", error);
+//   }
+// };
+
+// export default connectToDatabase;
+const myEnvVar = process.env.MONGODB_URI as string;
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(myEnvVar, { serverSelectionTimeoutMS: 50000 });
+    mongoose.connection.on("connected", () => {
+      console.log("Connected to MongoDB");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.error("Failed to connect to MongoDB", err);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.log("Disconnected from MongoDB");
+    });
+
+    await mongoose.connect(myEnvVar, {
+      serverSelectionTimeoutMS: 50000,
+    });
+
+    console.log("MongoDB connection established successfully");
   } catch (error) {
-    console.error("Failed to connect on database", error);
+    console.error("Failed to connect to MongoDB", error);
+    throw error; // Propager l'erreur pour la gestion ultérieure
   }
 };
 
