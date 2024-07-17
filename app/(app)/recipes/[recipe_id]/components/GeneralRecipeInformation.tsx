c6eb8e8f180dddbe509471bf351ca07860826651
Aurélie <aurelie.demure.ad@gmail.com>
2024-07-18 00:06:26 +0200

    feat(recipe): remove comment

--- app/(app)/recipes/[recipe_id]/components/GeneralRecipeInformation.tsx
+++ app/(app)/recipes/[recipe_id]/components/GeneralRecipeInformation.tsx
@@ -5,47 +5,38 @@ import { RecipePopulated } from "@/validators/recipe";
 import { ColorPalette } from "@/validators/tag";
 import { Clock12, CookingPot, Microwave, Thermometer } from "lucide-react";
 
-// type GeneralRecipeInformationProps = {
-//   recipe: RecipePopulated | null;
-// };
-const GeneralRecipeInformation = () =>
-  //   {
-  //   recipe,
-  // }: GeneralRecipeInformationProps
-  {
-    const { recipe } = useRecipe();
-    return (
-      <>
-        <div className="flex flex-col justify-center gap-4 md:w-1/2">
-          <div className="flex flex-col gap-2">
-            {recipe?.preparationTime && (
-              <p className="flex items-center gap-2">
-                <Clock12 />
-                <span>Temps de préparation :</span>
-                <span className="font-medium">
-                  {recipe.preparationTime} min
-                </span>
-              </p>
-            )}
-            {recipe?.cookingTime && (
-              <p className="flex items-center gap-2">
-                <CookingPot />
-                <span>Temps de cuisson : </span>
+const GeneralRecipeInformation = () => {
+  const { recipe } = useRecipe();
+  return (
+    <>
+      <div className="flex flex-col justify-center gap-4 md:w-1/2">
+        <div className="flex flex-col gap-2">
+          {recipe?.preparationTime && (
+            <p className="flex items-center gap-2">
+              <Clock12 />
+              <span>Temps de préparation :</span>
+              <span className="font-medium">{recipe.preparationTime} min</span>
+            </p>
+          )}
+          {recipe?.cookingTime && (
+            <p className="flex items-center gap-2">
+              <CookingPot />
+              <span>Temps de cuisson : </span>
 
-                <span className="font-medium">{recipe.cookingTime} min</span>
-              </p>
-            )}
-            {recipe?.ovenTemperature && (
-              <p className="flex items-center gap-2">
-                <Thermometer />
-                <span>Température du four : </span>
-                <span className="font-medium">{recipe.ovenTemperature}°</span>
-              </p>
-            )}
-          </div>
+              <span className="font-medium">{recipe.cookingTime} min</span>
+            </p>
+          )}
+          {recipe?.ovenTemperature && (
+            <p className="flex items-center gap-2">
+              <Thermometer />
+              <span>Température du four : </span>
+              <span className="font-medium">{recipe.ovenTemperature}°</span>
+            </p>
+          )}
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+};
 
 export default GeneralRecipeInformation;
