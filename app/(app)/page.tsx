e99611c6a299fd6ee4bcb310ca2bbae108495481
Aurélie <aurelie.demure.ad@gmail.com>
2024-07-23 01:14:08 +0200

    feat(recipes): view-pinned-recipes

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -5,15 +5,20 @@ import CategoriesList from "./components/CategoriesList";
 import TagsList from "./components/TagsList";
 
 import { findTags } from "@/database/tags/tag.repository";
-import { findRecipes } from "@/database/recipes/recipe.repository";
+import {
+  findPinnedRecipes,
+  findRecipes,
+} from "@/database/recipes/recipe.repository";
 import LoadingSvg from "@/assets/svg/LoadingSvg";
 import TagsProvider from "@/context/tags/provider";
+import PinnedRecipesList from "./components/PinnedRecipesList";
 
 const HomePage = async () => {
   await connectToDatabase();
   const categories = await findCategories();
   const tags = await findTags();
   const recipes = await findRecipes();
+  const pinnedRecipes = await findPinnedRecipes();
 
   return (
     <main className="flex flex-col items-center gap-8 p-6">
@@ -22,6 +27,7 @@ const HomePage = async () => {
         initialCategories={categories.length === 0 ? [] : categories}
       />
       <TagsList initialTags={tags.length === 0 ? [] : tags} recipes={recipes} />
+      <PinnedRecipesList pinnedRecipes={pinnedRecipes} />
     </main>
   );
 };
