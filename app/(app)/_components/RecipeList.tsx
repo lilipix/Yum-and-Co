8acd04a44986e05a1662fe69c1e934b89bc8aac7
Aurélie <aurelie.demure.ad@gmail.com>
2024-10-26 17:35:50 +0200

    feat(recipes): add picture on recipe card

--- app/(app)/_components/RecipeList.tsx
+++ app/(app)/_components/RecipeList.tsx
@@ -6,9 +6,10 @@ import { CardContent } from "@/components/ui/card";
 import { RecipePopulated } from "@/validators/recipe";
 import { Badge } from "@/components/ui/badge";
 import { ColorPalette, Tag } from "@/validators/tag";
-import { PinIcon, PinOff } from "lucide-react";
+import { Image as ImageIcon, PinIcon, PinOff } from "lucide-react";
 import useTogglePin from "@/hooks/useTogglePin";
 import { Category } from "@/validators/category";
+import Image from "next/image";
 
 type RecipeProps = {
   initialRecipes: RecipePopulated[];
@@ -20,11 +21,11 @@ const RecipeList = ({ initialRecipes, categoryId, tagsId }: RecipeProps) => {
   const { recipeList, handleTogglePin } = useTogglePin({ initialRecipes });
 
   return (
-    <CardContent>
+    <div>
       <ul className="flex flex-wrap gap-4">
         {recipeList.map((recipe) => (
           <li
-            className="mx-auto flex w-[200px] cursor-pointer flex-wrap rounded-xl bg-pinklight px-4 py-2 font-semibold transition duration-150 ease-in-out hover:border-pinklight hover:bg-pinkMedium"
+            className="mx-auto flex w-[300px] cursor-pointer flex-wrap rounded-xl bg-pinklight px-4 py-2 font-semibold transition duration-150 ease-in-out hover:border-pinklight hover:bg-pinkMedium"
             key={recipe.id}
           >
             <Link
@@ -37,19 +38,43 @@ const RecipeList = ({ initialRecipes, categoryId, tagsId }: RecipeProps) => {
               }
               className="block w-full"
             >
-              <div className="flex items-start justify-between gap-2">
-                <div>{recipe.title}</div>
+              <div className="mb-2 flex items-start justify-between gap-2">
+                <div className="flex-column">
+                  <h2>{recipe.title}</h2>
+                  <h3 className="text-sm font-light">{recipe.category.name}</h3>
+                </div>
                 <button
                   onClick={(e) => {
                     e.preventDefault();
                     handleTogglePin(recipe.id, recipe.pinned);
                   }}
-                  className="rounded-full bg-border p-2 hover:bg-border-dark"
+                  className="rounded-full pt-2 hover:bg-border-dark"
                   aria-label={recipe.pinned ? "Unpin Recipe" : "Pin Recipe"}
                 >
-                  {recipe.pinned ? <PinIcon size="16" /> : <PinOff size="16" />}
+                  {recipe.pinned ? (
+                    <PinIcon size="16" fill="black" />
+                  ) : (
+                    <PinOff size="16" />
+                  )}
                 </button>
               </div>
+              {recipe?.picture ? (
+                <div className="relative mb-6 h-[150px] w-full">
+                  <Image
+                    className="p-O rounded-lg shadow-lg"
+                    src={recipe?.picture}
+                    fill={true}
+                    style={{ objectFit: "cover" }}
+                    sizes="100vw"
+                    alt="Picture of the recipe"
+                  />
+                </div>
+              ) : (
+                <div className="relative mb-6 flex h-[150px] w-full flex-col items-center justify-center rounded-lg bg-gray-300 text-sm text-gray-400">
+                  <ImageIcon className="h-[100px] w-[100px]" />
+                  <p>Pas d&apos;image disponible.</p>
+                </div>
+              )}
               <div className="mt-2 flex w-full flex-wrap gap-2">
                 {recipe.tags.map((tag) => (
                   <div key={tag.id}>
@@ -63,7 +88,7 @@ const RecipeList = ({ initialRecipes, categoryId, tagsId }: RecipeProps) => {
           </li>
         ))}
       </ul>
-    </CardContent>
+    </div>
   );
 };
 
