da3e33c59284200e27e89ad05120f34433299d19
Aurélie <aurelie.demure.ad@gmail.com>
2026-02-17 19:46:23 +0100

    fix categories

--- database/categories/category.repository.ts
+++ database/categories/category.repository.ts
@@ -7,89 +7,69 @@ import { error } from "console";
 export const createCategory = async (
   data: CreateCategoryDTO,
 ): Promise<Category> => {
-  try {
-    const document = await CategoryModel.create(data);
-    return document.toJSON({
-      //serialized ObjectId to string
-      flattenObjectIds: true,
-      //__v non-inclusion
-      versionKey: false,
-    });
-  } catch (error) {
-    throw error;
-  }
+  const document = await CategoryModel.create(data);
+  return document.toJSON({
+    //serialized ObjectId to string
+    flattenObjectIds: true,
+    //__v non-inclusion
+    versionKey: false,
+  });
 };
 
 export const findCategories = async (): Promise<Category[]> => {
-  try {
-    const documents = await CategoryModel.find();
-    if (documents.length === 0) {
-      return [];
-    }
-    return documents.map((document) =>
-      document.toJSON({
-        //serialized ObjectId to string
-        flattenObjectIds: true,
-        //__v non-inclusion
-        versionKey: false,
-      }),
-    );
-  } catch (error) {
-    throw error;
+  const documents = await CategoryModel.find();
+  if (documents.length === 0) {
+    return [];
   }
-};
-
-export const findCategoryById = async (id: string): Promise<Category> => {
-  try {
-    const document = await CategoryModel.findById(id);
-
-    if (!document) {
-      throw new Error(`Category with id ${id} not found`);
-    }
-    return document.toJSON({
+  return documents.map((document) =>
+    document.toJSON({
       //serialized ObjectId to string
       flattenObjectIds: true,
       //__v non-inclusion
       versionKey: false,
-    });
-  } catch (error) {
-    throw new Error("Failed to find category by id");
+    }),
+  );
+};
+
+export const findCategoryById = async (id: string): Promise<Category> => {
+  const document = await CategoryModel.findById(id);
+
+  if (!document) {
+    throw new Error(`Category with id ${id} not found`);
   }
+  return document.toJSON({
+    //serialized ObjectId to string
+    flattenObjectIds: true,
+    //__v non-inclusion
+    versionKey: false,
+  });
 };
 
 export const updateCategory = async (
   id: string,
   data: UpdateCategoryDTO,
 ): Promise<Category> => {
-  try {
-    const document = await CategoryModel.findByIdAndUpdate(
-      id,
-      { $set: { ...data } },
-      { new: true },
-    );
-    if (!document) {
-      throw new Error("Category not found");
-    }
-    return document.toJSON({
-      flattenObjectIds: true,
-      versionKey: false,
-    });
-  } catch (error) {
-    throw new Error("Failed to update category");
+  const document = await CategoryModel.findByIdAndUpdate(
+    id,
+    { $set: { ...data } },
+    { new: true },
+  );
+  if (!document) {
+    throw new Error("Category not found");
   }
+  return document.toJSON({
+    flattenObjectIds: true,
+    versionKey: false,
+  });
 };
 
 export const deleteCategory = async (id: string): Promise<Category> => {
-  try {
-    const document = await CategoryModel.findByIdAndDelete(id);
-    if (!document) {
-      throw new Error("Category not found");
-    }
-    return document.toJSON({
-      flattenObjectIds: true,
-      versionKey: false,
-    });
-  } catch (error) {
-    throw Error("Failed to delete category");
+  const document = await CategoryModel.findByIdAndDelete(id);
+  if (!document) {
+    throw new Error("Category not found");
   }
+  return document.toJSON({
+    flattenObjectIds: true,
+    versionKey: false,
+  });
 };
--- database/tags/tag.repository.ts
+++ database/tags/tag.repository.ts
@@ -4,86 +4,66 @@ import connectToDatabase from "@/lib/mongodb";
 import TagModel from "./tag.model";
 
 export const createTag = async (data: CreateTagDTO): Promise<Tag> => {
-  try {
-    const document = await TagModel.create(data);
-    return document.toJSON({
+  const document = await TagModel.create(data);
+  return document.toJSON({
+    //serialized ObjectId to string
+    flattenObjectIds: true,
+    //__v non-inclusion
+    versionKey: false,
+  });
+};
+
+export const findTags = async (): Promise<Tag[]> => {
+  const documents = await TagModel.find();
+  return documents.map((document) =>
+    document.toJSON({
       //serialized ObjectId to string
       flattenObjectIds: true,
       //__v non-inclusion
       versionKey: false,
-    });
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const findTags = async (): Promise<Tag[]> => {
-  try {
-    const documents = await TagModel.find();
-    return documents.map((document) =>
-      document.toJSON({
-        //serialized ObjectId to string
-        flattenObjectIds: true,
-        //__v non-inclusion
-        versionKey: false,
-      }),
-    );
-  } catch (error) {
-    throw error;
-  }
+    }),
+  );
 };
 
 export const findTagByIds = async (tagIds: string[]): Promise<Tag[]> => {
-  try {
-    const documents = await TagModel.find({ _id: { $in: tagIds } });
-    return documents.map((doc) =>
-      doc.toJSON({
-        //serialized ObjectId to string
-        flattenObjectIds: true,
-        //__v non-inclusion
-        versionKey: false,
-      }),
-    );
-  } catch (error) {
-    throw new Error("Failed to find tag by id");
-  }
+  const documents = await TagModel.find({ _id: { $in: tagIds } });
+  return documents.map((doc) =>
+    doc.toJSON({
+      //serialized ObjectId to string
+      flattenObjectIds: true,
+      //__v non-inclusion
+      versionKey: false,
+    }),
+  );
 };
 
 export const updateTag = async (
   id: string,
   data: UpdateTagDTO,
 ): Promise<Tag> => {
-  try {
-    const document = await TagModel.findByIdAndUpdate(
-      id,
-      // update the document with the new data
-      { $set: { ...data } },
-      // return the updated document
-      { new: true },
-    );
-    if (!document) {
-      throw new Error("Tag not found");
-    }
-    return document.toJSON({
-      flattenObjectIds: true,
-      versionKey: false,
-    });
-  } catch (error) {
-    throw error;
+  const document = await TagModel.findByIdAndUpdate(
+    id,
+    // update the document with the new data
+    { $set: { ...data } },
+    // return the updated document
+    { new: true },
+  );
+  if (!document) {
+    throw new Error("Tag not found");
   }
+  return document.toJSON({
+    flattenObjectIds: true,
+    versionKey: false,
+  });
 };
 
 export const deleteTag = async (id: string): Promise<Tag> => {
-  try {
-    const document = await TagModel.findByIdAndDelete(id);
-    if (!document) {
-      throw new Error("Tag not found");
-    }
-    return document.toJSON({
-      flattenObjectIds: true,
-      versionKey: false,
-    });
-  } catch (error) {
-    throw Error("Failed to delete tag");
+  const document = await TagModel.findByIdAndDelete(id);
+  if (!document) {
+    throw new Error("Tag not found");
   }
+  return document.toJSON({
+    flattenObjectIds: true,
+    versionKey: false,
+  });
 };
